82192a3186addee5e0e750e4ba3eacb1d9be14ee
omar <omar.tawab95@gmail.com>
2018-04-13 17:39:43 +0200

    baseline model

--- models/baseline.py
+++ models/baseline.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 from utils.logger import Logger
+from utils.model import *
 
 
 class BaselineModel:
-    def __init__(self, config):
+    def __init__(self, embeddings, config):
         self.config = config
+        self.embeddings = embeddings
 
         self.init_placeholders()
 
@@ -32,23 +34,112 @@ class BaselineModel:
             self.cur_epoch_assign_op = self.cur_epoch_tensor.assign(self.cur_epoch_input)
 
     def init_placeholders(self):
-        with tf.name_scope('inputs'):
-            pass
+        self.context_placeholder = tf.placeholder(tf.int32, shape=(None, None))
+        self.context_mask_placeholder = tf.placeholder(tf.bool, shape=(None, None))
+        self.question_placeholder = tf.placeholder(tf.int32, shape=(None, None))
+        self.question_mask_placeholder = tf.placeholder(tf.bool, shape=(None, None))
+
+        self.answer_span_start_placeholder = tf.placeholder(tf.int32)
+        self.answer_span_end_placeholder = tf.placeholder(tf.int32)
+
+        self.max_context_length_placeholder = tf.placeholder(tf.int32)
+        self.max_question_length_placeholder = tf.placeholder(tf.int32)
+        self.dropout_placeholder = tf.placeholder(tf.float32)
 
     def setup_embeddings(self):
-        pass
+        with tf.variable_scope("embeddings"):
+            if self.config.training.retrain_embeddings:
+                embeddings = tf.get_variable("embeddings", initializer=self.embeddings)
+            else:
+                embeddings = tf.cast(self.embeddings, dtype=tf.float32)
+
+            self.question_embeddings = self._embedding_lookup(embeddings, self.question_placeholder,
+                                                              self.max_question_length_placeholder)
+            self.context_embeddings = self._embedding_lookup(embeddings, self.context_placeholder,
+                                                             self.max_context_length_placeholder)
+
+    def _embedding_lookup(self, embeddings, indicies, max_length):
+        embeddings = tf.nn.embedding_lookup(embeddings, indicies)
+        embeddings = tf.reshape(embeddings, shape=[-1, max_length, self.config.embedding_size])
+        return embeddings
+
+    def add_preds_op(self):
+        with tf.variable_scope("q"):
+            Hq, (q_final_state_fw, q_final_state_bw) = self.encode(self.question_embeddings,
+                                                                   self.question_mask_placeholder)
+
+        if self.config.model.share_encoder_weights:
+            with tf.variable_scope("q"):
+                Hc, (_, _) = self.encode(self.context_embeddings,
+                                         self.context_mask_placeholder,
+                                         initial_state_fw=q_final_state_fw,
+                                         initial_state_bw=q_final_state_bw,
+                                         reuse=True)
+        else:
+            with tf.variable_scope("c"):
+                Hc, (_, _) = self.encode(self.context_embeddings,
+                                         self.context_mask_placeholder,
+                                         initial_state_fw=q_final_state_fw,
+                                         initial_state_bw=q_final_state_bw)
+
+        with tf.variable_scope("decoding"):
+            start, end = self.decode(Hc)
+        self.preds = start, end
 
     def add_loss_op(self):
-        pass
+        with tf.variable_scope("loss"):
+            answer_span_start_one_hot = tf.one_hot(self.answer_span_start_placeholder, self.max_context_length_placeholder)
+            answer_span_end_one_hot = tf.one_hot(self.answer_span_end_placeholder, self.max_context_length_placeholder)
+
+            start, end = self.preds
+            loss1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=start, labels=answer_span_start_one_hot))
+            loss2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=end, labels=answer_span_end_one_hot))
+            self.loss = loss1 + loss2
 
     def add_optimization_op(self):
-        pass
+        variables = tf.trainable_variables()
+        gradients = tf.gradients(self.loss, variables)
+        gradients, _ = tf.clip_by_global_norm(gradients, self.config.training.max_grad_norm)
 
-    def encode(self):
-        pass
+        if self.config.learning_rate_annealing:
+            global_step = tf.Variable(0, trainable=False)
+            learning_rate = tf.train.exponential_decay(self.config.learning_rate, global_step, 1250, 0.96,
+                                                       staircase=False)
+            global_step = tf.add(1, global_step)
+        else:
+            learning_rate = self.config.learning_rate
 
-    def decode(self):
-        pass
+        optimizer = get_optimizer(self.config.optimizer, learning_rate)
+        train_op = optimizer.apply_gradients(zip(gradients, variables))
+
+        # For applying EMA for trained parameters
+        if self.config.ema_for_weights:
+            ema = tf.train.ExponentialMovingAverage(0.999)
+            ema_op = ema.apply(variables)
+
+            with tf.control_dependencies([train_op]):
+                train_op = tf.group(ema_op)
+
+        return train_op
+
+    def encode(self, inputs, masks, initial_state_fw=None, initial_state_bw=None, reuse=False):
+        output_concat, (final_state_fw, final_state_bw) = BiLSTM(inputs, masks, self.config.model.hidden_size, initial_state_fw,
+                                                                 initial_state_bw, self.dropout_placeholder, reuse)
+
+        return output_concat, (final_state_fw, final_state_bw)
+
+    def decode(self, inputs):
+        with tf.variable_scope("start"):
+            start = logits_helper(inputs, self.max_context_length_placeholder)
+            start = prepro_for_softmax(start, self.context_mask_placeholder)
+
+        with tf.variable_scope("end"):
+            end = logits_helper(inputs, self.max_context_length_placeholder)
+            end = prepro_for_softmax(end, self.context_mask_placeholder)
+
+        return start, end
 
     def build_model(self):
-        pass
+        self.add_preds_op()
+        self.add_loss_op()
+        self.add_optimization_op()
